476dadee3e2a579771b344ee095aeca2ff357c3c
Yesenia López Giraldo <yesenialopezg07@gmail.com>
2021-04-28 10:44:26 -0500

    correo

--- app/controllers/estudiates/estudiantes.controller.js
+++ app/controllers/estudiates/estudiantes.controller.js
@@ -1,4 +1,4 @@
-//const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 const excel = require("exceljs");
 const PostgresService = require('../../services/postgres.service');
 const _pg = new PostgresService();
@@ -19,13 +19,15 @@ const createUser = async (req, res) => {
         let estudiante = req.body;
         let sql = `insert into personas (id, name, email) values(${estudiante.id}, '${estudiante.name}', '${estudiante.email}')`;
         let result = await _pg.executeSql(sql);
-        /**if (result.rowCount == 1) {
+        let email = estudiante.email;
+        if (result.rowCount == 1) {
             try {
-                emailConfirmacion(estudiante.email);
+                let enviado = await crearEmail(email);
+                console.log(enviado);
             } catch (error) {
                 console.log(error);
             }
-        }**/
+        }
         return res.send({ ok: result.rowCount == 1, message: result == 1 ? "El estudiante no fue creado" : "Estudiante creado", content: estudiante, });
     } catch (error) {
         return res.send({ ok: false, message: "Error creado el estudiante", content: error, });
@@ -33,24 +35,45 @@ const createUser = async (req, res) => {
 
 };
 
-/**async function emailConfirmacion (req) {
+async function crearEmail(email) {
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 465,
-        secure: true, 
+        secure: true,
         auth: {
-          user: 'pruebas12correos@gmail.com', 
-          pass: 'mzvuqumstzwkveor',
+            user: 'pruebas12correos@gmail.com',
+            pass: 'mzvuqumstzwkveor',
         },
-      });
+    });
 
-      let info = await transporter.sendMail({
-        from: '"Pruebas correos" <pruebas12correos@gmail.com>', 
-        to: req,
-        text: "Confirmacion de prueba", 
-        html: "<b>Hello world?</b>", 
-      });
-}; emailConfirmacion().catch(console.error);**/
+    let info = await transporter.sendMail({
+        from: '"Correos de confirmacion" <pruebas12correos@gmail.com>',
+        to: email,
+        subject: "Ahora eres parte de nuestra Universidad",
+        text: "Confirmacion de prueba",
+        html: `
+<head>
+  <style>
+    h1 { color: #2C8B78; }
+    h3 { color: #F9AC36; }
+    pr { color: #206355; }
+   	b { color: #206355; }
+  </style>
+</head>
+
+<body>
+<center>
+  <br>
+  <h1>BIENVENIDO A NUESTRA UNIVERSIDAD</h1>
+<b>Le informamos que ha sido registrado correctamente a nuestro sistema</b> 
+  <br>
+  <br>
+  <pr>Ahora podrá disfrutar de todos los beneficios que tiene la Universidad para tí</pr>
+  <h3>¡NO TE PIERDAS DE NADA!</h3>
+</center>
+</body>`,
+    });
+};
 
 function crearExcel(req, res) {
     let workbook = new excel.Workbook();
@@ -64,14 +87,14 @@ function crearExcel(req, res) {
 
     worksheet.addRows(req);
 
-      res.setHeader(
+    res.setHeader(
         "Content-Disposition",
         "attachment; filename=" + "Estudiantes.xlsx"
-      );
-      
-      return workbook.xlsx.write(res).then(function () {
+    );
+
+    return workbook.xlsx.write(res).then(function () {
         res.status(200).end();
-      });
+    });
 }
 
 module.exports = { getUsers, createUser };
--- readme.md
+++ readme.md
@@ -1,23 +1,24 @@
+ACTIVIDAD 2 - DESARROLLO WEB // ABRIL 28 DEL 2021
+
 OBJETIVO: Analizar y compreder la estructura de un API.
 
 OBJETIVOS ESPECIFICOS:
-
 Identificar los diferentes endpoints que se pueden tener en un proyecto.
 Diferenciar los http methods 
 Construir adecuadamente una petición http request con sus componentes.
-SCRIPT BASE DE DATOS
 
+SCRIPT BASE DE DATOS
 create database universidad;
 create table personas ( id serial not null primary key, name varchar not null,  email varchar not null);
-REQUISITOS DE LA ENTREGA:
 
+REQUISITOS DE LA ENTREGA:
 Crear la base de datos universidad y la tabla de personas.
 [endpoint #1] Hacer un endpoint que me permita crear una persona en la base de datos.
 Enviar un correo de bienvenida a la persona después de que se guarde en base de datos. (La plantilla y la información del correo es diseño libre)
 [endpoint #2] Descargar un reporte en csv o excel con la información de la tabla de personas.
 Entregar la colección de postman con cada uno de los enpoints solicitados.
 Entregar el link del repositorio.
-Líbrerias: A continuación se compartes unas librerías opcionales, pueden crear su propia librería o utilizar otras.
 
+Líbrerias: A continuación se compartes unas librerías opcionales, pueden crear su propia librería o utilizar otras.
 https://nodemailer.com/
 https://www.npmjs.com/package/exceljs
